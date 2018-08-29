21748432f587e9054f6b5561710307691a43c577
Jose Ramon <joseramon@geografiafisica.org>
2018-08-29 08:34:45 -0400

    script generalization

--- aplicacion_transformacion_homogeneizacion_inversa.R
+++ aplicacion_transformacion_homogeneizacion_inversa.R
@@ -18,7 +18,7 @@ shapiro.test(d$trans)
 dprht <-d[,-4]#Prepare the data
 write.table(dprht, 'dprht.csv', row.names = F, col.names = F, sep = ',')
 FindU(InSeries = 'dprht.csv', MissingValueCode = "NA", output = 'homogenized')
-#In this case there are no changepoints, so in the homogenized series will show the same values as the input series, which is also the transformed one
+#In this case there are no changepoints, so the homogenized series will show the same values as the input series, which is also the transformed one
 hom <- read.table('homogenized_U.dat')
 #The inverse transformation shows the same values as the original series
 d$inverse <- (hom$V9*lam + 1)^(1/lam) - 0.01
